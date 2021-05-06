05526cc3137683929515ff7348fd3a468a3cd8c4
TMcMac <timmcmackenjr@gmail.com>
2021-05-06 15:40:45 +0000

    Remembering how binary trees work

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -10,18 +10,15 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_node;
-
-	if (parent == NULL || *parent == NULL)
-		return (NULL);
-
-	new_node = malloc(sizeof(binary_tree_t) * 1);
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t) * 1);
 
 	if (new_node == NULL)
-		return (NULL);
-
+	  return (NULL);
+	
 	new_node->n = value;
-	parent->next = new_node;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
 
 	return (new_node);
 }
