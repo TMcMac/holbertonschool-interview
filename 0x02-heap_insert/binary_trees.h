ba7df1225c3d9da9302260f0c80d159542850dba
TMcMac <timmcmackenjr@gmail.com>
2021-05-06 15:48:34 +0000

    Now it compiles

--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -2,6 +2,8 @@
 #define _BINARY_TREES_H_
 
 #include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -11,19 +13,19 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
 	int n;
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-};
+} binary_tree_t;
 
 /*Max Binary Heap */
 typedef struct binary_tree_s heap_t;
 
 /*Prototypes*/
 void binary_tree_print(const binary_tree_t *);
-
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* _BINARY_TREES_H_ */
