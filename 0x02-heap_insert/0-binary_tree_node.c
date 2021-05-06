6cae6c2aaac8999b384c6044d95f099a5a962197
Tim <timmcmackenjr@gmail.com>
2021-05-06 10:23:24 -0500

    Converts spaces to tabs

--- 0x02-heap_insert/0-binary_tree_node.c
+++ 0x02-heap_insert/0-binary_tree_node.c
@@ -10,18 +10,18 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-  binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-  if (parent == NULL || *parent == NULL)
-    return (NULL);
+	if (parent == NULL || *parent == NULL)
+		return (NULL);
 
-  new_node = malloc(sizeof(binary_tree_t) * 1);
+	new_node = malloc(sizeof(binary_tree_t) * 1);
 
-  if (new_node == NULL)
-    return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-  new_node->n = value;
-  parent->next = new_node;
+	new_node->n = value;
+	parent->next = new_node;
 
-  return (new_node);
+	return (new_node);
 }
