597cb34b27ab7dba2273b19a72250fd3bda2f79f
TMcMac <timmcmackenjr@gmail.com>
2021-05-07 00:20:49 +0000

    Adds 1

---
+++ 0x02-heap_insert/1-heap_insert.c
@@ -0,0 +1,27 @@
+#include <binary_trees.h>
+
+/**
+ * heap_insert - a function for inserting into a binary tree
+ * based on the max heap ordering
+ * @root: a double pointer to a root node
+ * @value: the value to be inserted into the new node
+ * Return: the address of the new node if successful
+ */
+
+heap_t *heap_insert(heap_t **root, int value)
+{
+  heap_t *new_node;
+  heap_t *position = *root;
+  
+  if (root == NULL)
+    return (NULL);
+
+  if (*root == NULL)
+    {
+      new_node = binary_tree_node(NULL, value);
+      *root = new_node;
+      return(*root);
+    }
+
+  return(NULL)
+}
--- 0x02-heap_insert/binary_trees.h
+++ 0x02-heap_insert/binary_trees.h
@@ -27,5 +27,6 @@ typedef struct binary_tree_s heap_t;
 /*Prototypes*/
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
 
 #endif /* _BINARY_TREES_H_ */
