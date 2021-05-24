bcc0e9735f90728f300fba0e145e43318deeecef
TMcMac <timmcmackenjr@gmail.com>
2021-05-24 19:27:12 +0000

    Initial commit

---
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * is_palindrome: a function to check if a singly linked
+ * list is or is not a palindrome (same forwars and backwards)
+ * @head: a double pointer to the head (node 0) of the list
+ * Return: 1 ifthe list is a Plindrome or 0 if it isnt
+ */
+
+int is_palindrome(listint_t **head)
+{
+  int count = 0;
+  int i = 0;
+  listint_t *mover;
+  int *listn;
+  
+  if (!head || !*head)
+      return (0);
+
+  mover = *head;
+  if (mover->next == NULL)
+      return (1);
+
+  while (mover != NULL)
+    {
+      count++;
+      mover = mover->next;
+    }
+  printf("Transversed List\n Count = %d\n", count);
+  listn = malloc(sizeof(int) * count);
+  if (listn == NULL)
+      return (0);
+  printf("Listn malloced\n");
+  mover = *head;
+  while (mover != NULL)
+    {
+      listn[i] = mover->n;
+      i++;
+      mover = mover->next;
+    }
+  printf("Listn populated\n");
+  mover = *head;
+  while (mover!= NULL)
+    {
+      printf("Comparing list[n] = %d AND mover[n] = %d\n", listn[i - 1], mover->n);
+      if (listn[i - 1] == mover->n)
+	{
+	  i--;
+	  mover = mover->next;
+	}
+      else
+	  return (0);
+    }
+  return (1);
+}
