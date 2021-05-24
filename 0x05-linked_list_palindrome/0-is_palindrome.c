0e80e3e5442bb78e37b876d86fb160cdf87edf71
TMcMac <timmcmackenjr@gmail.com>
2021-05-24 21:03:34 +0000

    betty fixes and removes print statements

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * is_palindrome: a function to check if a singly linked
+ * is_palindrome - a function to check if a singly linked
  * list is or is not a palindrome (same forwars and backwards)
  * @head: a double pointer to the head (node 0) of the list
  * Return: 1 ifthe list is a Plindrome or 0 if it isnt
@@ -13,7 +13,7 @@ int is_palindrome(listint_t **head)
 	int i = 0;
 	listint_t *mover;
 	int *listn;
-	
+
 	if (!head || !*head)
 		return (0);
 
@@ -26,11 +26,9 @@ int is_palindrome(listint_t **head)
 		count++;
 		mover = mover->next;
 	}
-	printf("Transversed List\n Count = %d\n", count);
 	listn = malloc(sizeof(int) * count);
 	if (listn == NULL)
 		return (0);
-	printf("Listn malloced\n");
 	mover = *head;
 	while (mover != NULL)
 	{
@@ -38,11 +36,9 @@ int is_palindrome(listint_t **head)
 		i++;
 		mover = mover->next;
 	}
-	printf("Listn populated\n");
 	mover = *head;
-	while (mover!= NULL)
+	while (mover != NULL)
 	{
-		printf("Comparing list[n] = %d AND mover[n] = %d\n", listn[i - 1], mover->n);
 		if (listn[i - 1] == mover->n)
 		{
 			i--;
