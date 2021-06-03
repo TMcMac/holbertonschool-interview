7f558f009b0ca5875621fa875dc2aa1bf530df48
TMcMac <timmcmackenjr@gmail.com>
2021-06-03 03:37:16 +0000

    Adds a basic test for foramting

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -25,6 +25,8 @@ def parse_data():
                 continue
             count += 1
             parsing = line.split()  # Break up line by spaces
+            if not (parsing[-1].isnumeric()) or not (parsing[-2].isnumeric()):
+                continue
             filesizes.append(int(parsing[-1]))  # last element is filesize
             if int(parsing[-2]) in statuscodes:
                 # second to last is status code
