8840da1b5cde60de347051471e33f7db04546280
TMcMac <timmcmackenjr@gmail.com>
2021-06-03 03:02:41 +0000

    More testing and a tests folder

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -13,25 +13,32 @@ def parse_data():
     the program will output a total size of the summed file sizes
     and a tally of the status codes we received.
     """
-    count = 0
-    codes = []
-    statuscodes = [200, 301, 400, 401, 403, 404, 405, 500]
-    filesizes = []
+
+    count = 0 # Tracks 10 iterations for printing
+    codes = [] # All valid status codes received
+    statuscodes = [200, 301, 400, 401, 403, 404, 405, 500] # Valid codes
+    filesizes = [] # list of file sizes received
     
     try:
-        for line in sys.stdin:
-            if line == "":
+        for line in sys.stdin: # Reading lines from stdin
+            if line == "": # if empty line, skip
                 continue
             count += 1
-            parsing = line.split()
-            filesizes.append(int(parsing[-1]))
-            if int(parsing[-2]) in statuscodes:
+            parsing = line.split() # Break up line by spaces
+            filesizes.append(int(parsing[-1])) # last element is filesize
+            if int(parsing[-2]) in statuscodes: # second to last is status code
                 codes.append(int(parsing[-2]))
-            if count % 10 == 0:
-                print("File size: {}".format(sum(filesizes)))
-                codes_dict = {i: codes.count(i) for i in codes}
+            if count % 10 == 0: # Every ten rounds print data
+                print("File size: {}".format(sum(filesizes))) # sumall file sizes
+                codes_dict = {i: codes.count(i) for i in codes} # dict of codes and count
                 for k, v in codes_dict.items():
                     print("{}: {}".format(k, v))
+        # we leave the loop if we run out of lines, so final print
+        print("File size: {}".format(sum(filesizes)))
+        codes_dict = {i: codes.count(i) for i in codes}
+        for k, v in codes_dict.items():
+            print("{}: {}".format(k, v))
+    # If a ctrl C is sent, final print and quit
     except KeyboardInterrupt:
         print("File size: {}".format(sum(filesizes)))
         codes_dict = {i: codes.count(i) for i in codes}
---
+++ 0x06-log_parsing/tests/testone.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import sys
+
+sys.stdout.write('127.0.0.1 - [2021.06.02 21:43:30:99] "GET /projects/260 HTTP/1.1" 200 777')
