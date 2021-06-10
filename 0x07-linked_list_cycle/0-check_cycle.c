323a74ece66b340cb518481e4577ec5ce7ec5b32
TMcMac <timmcmackenjr@gmail.com>
2021-06-10 20:15:31 +0000

    Problem solved

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+
+int check_cycle(listint_t *list)
+{
+  listint_t *hare = list, *tort = list;
+
+  if (list->next == NULL || list == NULL)
+    return(0);
+
+  while (hare != NULL && tort != NULL)
+    {
+      if (hare->next->next != NULL)
+	{
+	  hare = hare->next->next;
+	  tort = tort->next;
+	}
+      else
+	return(0);
+      if (tort == hare)
+	return(1);
+      
+    }
+  return(0);
+}
