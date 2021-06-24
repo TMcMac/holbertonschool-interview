5a084436d964e60afb8f79b7b1069e33d936ea43
Tim <timmcmackenjr@gmail.com>
2021-06-24 16:28:45 -0500

    pep8 fixes

--- 0x09-utf8_validation/0-validate_utf8.py
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -4,23 +4,24 @@
 
 def validUTF8(data):
     """
-	Function: validUTF8
-	Description:
-		Validates whether the integers in a list
-		represent valid UTF-8 characters.
-	Args:
-		data: list of ints
-	Return:
-		True if valid UTF8 chars or False if not
-	"""
+        Function: validUTF8
+        Description:
+            Validates whether the integers in a list
+            represent valid UTF-8 characters.
+        Args:
+            data: list of ints
+        Return:
+            True if valid UTF8 chars or False if not
+    """
 
     if len(data) == 0:
-       return True
+        return True
     if len(data) == 1:
-        if data[0] >= 32 and data[0] <= 126:
-            return True
-        else:
-            return False
+    
+    if data[0] >= 32 and data[0] <= 126:
+        return True
+    else:
+        return False
     for num in data:
         if num > 31 and num < 127:
             continue
