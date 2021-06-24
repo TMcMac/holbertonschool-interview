b66b0b4c3ee44343a9f35e17e88f1d4425b41d2c
TMcMac <timmcmackenjr@gmail.com>
2021-06-24 15:40:29 +0000

    Initial commit

--- 0x03-minimum_operations/0-main.py
+++ 0x03-minimum_operations/0-main.py
@@ -7,6 +7,18 @@ minOperations = __import__('0-minoperations').minOperations
 
 n = 4
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
-
+print("*************************************\n\n")
 n = 12
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("*************************************\n\n")
+n = 21
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("*************************************\n\n")
+n = 19170307
+print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+print("*************************************\n\n")
+#n = 30
+#print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+
+n = 2147483640
 print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
