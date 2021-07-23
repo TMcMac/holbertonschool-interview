05f3e3a99d93b0985645a8137505e9ac3e877eaf
TMcMac <timmcmackenjr@gmail.com>
2021-07-23 02:11:37 +0000

    Working version

--- 0x0C-nqueens/0-nqueens.py
+++ 0x0C-nqueens/0-nqueens.py
@@ -3,7 +3,7 @@
 import sys
 
 
-def nqueens(n):
+def nqueens(n, y, board):
     """
     Method: nqueens - place n queens
             on an n by n board so that
@@ -14,6 +14,25 @@ def nqueens(n):
     Return: All possible solutions to
             placement, in list of lists form
     """
+    for x in range(n):
+        hold = 0
+        for q in board:
+            if x == q[1]:
+                hold = 1
+                break
+            if y - x == q[0] - q[1]:
+                hold = 1
+                break
+            if x - q[1] == q[0] - y:
+                hold = 1
+                break
+        if hold == 0:
+            board.append([y, x])
+            if y != n - 1:
+                nqueens(n, y + 1, board)
+            else:
+                print(board)
+            del board[-1]
 
 
 def main():
@@ -29,6 +48,7 @@ def main():
         print("N must be at least 4")
         sys.exit(1)
 
+    nqueens(n, 0, [])
 
 if __name__ == '__main__':
     main()
