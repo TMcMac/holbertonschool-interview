41c83e295cb386b29a02e77e95ee02a77149b356
TMcMac <timmcmackenjr@gmail.com>
2021-07-23 01:54:48 +0000

    Intial commit

---
+++ 0x0C-nqueens/0-nqueens.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""Solving N Queens with Backtracing"""
+import sys
+
+
+def nqueens(n):
+    """
+    Method: nqueens - place n queens
+            on an n by n board so that
+            no queens are attacking any
+            others.
+    Parameters: n is an int that sets
+                board size and # of queens
+    Return: All possible solutions to
+            placement, in list of lists form
+    """
+
+
+def main():
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
+
+    
