515d1be95fc0fda7786d9444971fdbfc0ded1f7c
TMcMac <timmcmackenjr@gmail.com>
2021-08-09 17:50:51 +0000

    Swapping over to async

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+};
+
 const args = process.argv;
 
 if (args.length != 3) {
@@ -16,25 +20,35 @@ const film = Number(args[2]);
 const url = "https://swapi-api.hbtn.io/api/films/"
 const request = require('request');
 
+async function swapi() {
 request(url + film, function (error, response, body) {
-    console.error('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    if (error !== null) {
+	console.error('error:', error); // Print the error if one occurred
+    };
+    if (Number(response.statusCode) !== 200) {
+	console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    };
 //    console.log('body:', body); // Print the HTML for the SWAPI page.
     const payload = JSON.parse(body);
 //    console.log (typeof payload);
     const characters = payload["characters"];
-    let x = 0;
-    for (x < characters.length) {
-    	request(characters[x], function (error, response, body) {
+    console.log(characters);
+    for (let x = 0; x < characters.length; x++) {
+	request(characters[x], function (error, response, body) {
+	    await sleep(2000);
+	    if (error !== null) {
 	        console.error('errorCharData:', error); // Print the error if one occurred
-	        console.log('statusCodeCharData:', response && response.statusCode); // Print the response status code if a response was received
-	        let charData = JSON.parse(body);
-	        let charName = charData['name'];
-	        console.log(charName);
-	        x = x + 1;
-	    });
+	    };
+	    if (Number(response.statusCode) !== 200) {
+		console.log('statusCodeCharData:', response && response.statusCode); // Print the response status code if a response was received
+	    };
+	    console.log(characters[x]);
+	    let charData = JSON.parse(body);
+	    let charName = charData['name'];
+	    console.log(charName);
+	});
     };
 });
-
+};
 
 
