e43c06e337e9d58c8a270bb6aee044fe6520d5ac
TMcMac <timmcmackenjr@gmail.com>
2021-08-10 01:19:44 +0000

    Working sort of

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -37,7 +37,6 @@ request(url, function (error, response, body) {
     };
     for (let x = 0; x < characters.length; x++) {
 	    request(characters[x], async function (error, response, body) {
-		    const result = await resolveAfter2Seconds();
 		    if (error !== null) {
 				console.error('errorCharData:', error); // Print the error if one occurred
 			};
@@ -46,7 +45,8 @@ request(url, function (error, response, body) {
 			};
 			let charData = JSON.parse(body); // Parse string to dict
 			let charName = charData['name']; // Get just the character name
-		    console.log(charName);
-		});
+			    const result = await resolveAfter2Seconds();
+		console.log(charName);
+	    });
 	};
 });
