2acf8392ba73cc379820fe1bff38dc6d927a4d5e
TMcMac <timmcmackenjr@gmail.com>
2021-09-23 22:06:55 +0000

    Initial commit

---
+++ 0x15-infinite_multiplication/0-mul.c
@@ -0,0 +1,81 @@
+#include "holberton.h"
+/**
+ * main - Take in two numbers and attempt to
+ *        multiply them
+ * @argc: an int count of arguments
+ * @argv: an array of arguments 
+ * Return: 0
+ */
+
+int main(int argc, char **argv) {
+
+  int num1_len, num2_len;
+  long long int num1, num2;
+
+  if(argc != 3) {
+    printf("Error\n");
+    exit(98);
+  }
+
+  if ((_isdigit(argv[1]) == 0) && (_isdigit(argv[2]) == 0)) {
+	num1_len = _strlen(argv[1]);
+	num2_len = _strlen(argv[2]);
+  }
+  
+  if ((num1_len + num2_len) < 17) {
+    num1 = _atoi(argv[1], num1_len);
+    num2 = _atoi(argv[2], num2_len);
+    printf("%lld\n", num1 * num2);
+  }
+  
+  return (0);
+}
+
+
+/**
+ * _strlen - checks the length of the string
+ * @string: a string of digits, hopefully
+ * Return: the count of items in the string
+ */
+int _strlen(char *string) {
+  int i = 0;
+
+  while(string[i] != '\0') {
+    i++;
+  }
+  return (i - 1);
+}
+
+/**
+ * _isdigit - make sure all chars in a string are numeric
+ * @string: a string of chars
+ * Return: 0 if all digits
+ */
+int _isdigit(char *string) {
+  int i;
+
+  for(i = 0; string[i] != '\0'; i++) {
+    if((string[i] < 48) || (string[i] > 57)) {
+	printf("Error\n");
+	exit(98);
+      } else {
+	continue;
+    }}
+  return (0);
+}
+
+/**
+ * _atoi - convert a string to a number
+ * @string: a string of chars
+ * @len: the length of the string befor \0
+ * Return: the number as a long long
+ */
+long long int _atoi(char *string, int len) {
+  long long int num = 0;
+  int i;
+
+  for(i = 0; i <= len; i++) {
+    num = (num * 10) + (string[i] - '0');
+  }
+  return (num);
+}
---
+++ 0x15-infinite_multiplication/holberton.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int multi_long(char *num1, char *num2);
+int _strlen(char *string);
+int _isdigit(char *string);
+long long int _atoi(char *string, int len);
+
+#endif
