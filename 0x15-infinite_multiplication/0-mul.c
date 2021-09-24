699898fe7eed66d16bd26168d314a0e4e720e47d
TMcMac <timmcmackenjr@gmail.com>
2021-09-24 03:32:28 +0000

    Betty fixes

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -3,26 +3,30 @@
  * main - Take in two numbers and attempt to
  *        multiply them
  * @argc: an int count of arguments
- * @argv: an array of arguments 
+ * @argv: an array of arguments
  * Return: 0
  */
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
   int num1_len, num2_len;
   long int num1, num2;
 
-  if(argc != 3) {
+  if(argc != 3)
+  {
     printf("Error\n");
     exit(98);
   }
 
-  if ((_isdigit(argv[1]) == 0) && (_isdigit(argv[2]) == 0)) {
+  if ((_isdigit(argv[1]) == 0) && (_isdigit(argv[2]) == 0))
+  {
 	num1_len = _strlen(argv[1]);
 	num2_len = _strlen(argv[2]);
   }
   
-  if ((num1_len + num2_len) < 10) {
+  if ((num1_len + num2_len) < 10)
+  {
     num1 = _atoi(argv[1], num1_len);
     num2 = _atoi(argv[2], num2_len);
     printf("%ld\n", num1 * num2);
@@ -37,10 +41,12 @@ int main(int argc, char **argv) {
  * @string: a string of digits, hopefully
  * Return: the count of items in the string
  */
-int _strlen(char *string) {
+int _strlen(char *string)
+{
   int i = 0;
 
-  while(string[i] != '\0') {
+  while(string[i] != '\0')
+  {
     i++;
   }
   return (i - 1);
@@ -51,16 +57,20 @@ int _strlen(char *string) {
  * @string: a string of chars
  * Return: 0 if all digits
  */
-int _isdigit(char *string) {
+int _isdigit(char *string)
+{
   int i;
 
-  for(i = 0; string[i] != '\0'; i++) {
-    if((string[i] < 48) || (string[i] > 57)) {
+  for(i = 0; string[i] != '\0'; i++)
+  {
+    if((string[i] < 48) || (string[i] > 57))
+    {
 	printf("Error\n");
 	exit(98);
-      } else {
+    } else {
 	continue;
-    }}
+    }
+  }
   return (0);
 }
 
@@ -70,11 +80,13 @@ int _isdigit(char *string) {
  * @len: the length of the string befor \0
  * Return: the number as a long
  */
-long int _atoi(char *string, int len) {
+long int _atoi(char *string, int len)
+{
   long int num = 0;
   int i;
 
-  for(i = 0; i <= len; i++) {
+  for(i = 0; i <= len; i++)
+  {
     num = (num * 10) + (string[i] - '0');
   }
   return (num);
