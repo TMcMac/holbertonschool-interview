3cc23dec6d6f504feb7f30bcd68fd7ca4cf5a90c
Tim <timmcmackenjr@gmail.com>
2021-09-23 22:34:23 -0500

    Betty Fixes

--- 0x15-infinite_multiplication/0-mul.c
+++ 0x15-infinite_multiplication/0-mul.c
@@ -10,29 +10,29 @@
 int main(int argc, char **argv)
 {
 
-  int num1_len, num2_len;
-  long int num1, num2;
+	int num1_len, num2_len;
+	long int num1, num2;
 
-  if(argc != 3)
-  {
-    printf("Error\n");
-    exit(98);
-  }
+	if(argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-  if ((_isdigit(argv[1]) == 0) && (_isdigit(argv[2]) == 0))
-  {
+	if ((_isdigit(argv[1]) == 0) && (_isdigit(argv[2]) == 0))
+	{
 	num1_len = _strlen(argv[1]);
 	num2_len = _strlen(argv[2]);
-  }
-  
-  if ((num1_len + num2_len) < 10)
-  {
-    num1 = _atoi(argv[1], num1_len);
-    num2 = _atoi(argv[2], num2_len);
-    printf("%ld\n", num1 * num2);
-  }
-  
-  return (0);
+	}
+	
+	if ((num1_len + num2_len) < 10)
+	{
+		num1 = _atoi(argv[1], num1_len);
+		num2 = _atoi(argv[2], num2_len);
+		printf("%ld\n", num1 * num2);
+	}
+	
+	return (0);
 }
 
 
@@ -43,13 +43,13 @@ int main(int argc, char **argv)
  */
 int _strlen(char *string)
 {
-  int i = 0;
+	int i = 0;
 
-  while(string[i] != '\0')
-  {
-    i++;
-  }
-  return (i - 1);
+	while(string[i] != '\0')
+	{
+		i++;
+	}
+	return (i - 1);
 }
 
 /**
@@ -59,19 +59,19 @@ int _strlen(char *string)
  */
 int _isdigit(char *string)
 {
-  int i;
+	int i;
 
-  for(i = 0; string[i] != '\0'; i++)
-  {
-    if((string[i] < 48) || (string[i] > 57))
-    {
+	for(i = 0; string[i] != '\0'; i++)
+	{
+		if((string[i] < 48) || (string[i] > 57))
+		{
 	printf("Error\n");
 	exit(98);
-    } else {
+		} else {
 	continue;
-    }
-  }
-  return (0);
+		}
+	}
+	return (0);
 }
 
 /**
@@ -82,12 +82,12 @@ int _isdigit(char *string)
  */
 long int _atoi(char *string, int len)
 {
-  long int num = 0;
-  int i;
+	long int num = 0;
+	int i;
 
-  for(i = 0; i <= len; i++)
-  {
-    num = (num * 10) + (string[i] - '0');
-  }
-  return (num);
+	for(i = 0; i <= len; i++)
+	{
+		num = (num * 10) + (string[i] - '0');
+	}
+	return (num);
 }
