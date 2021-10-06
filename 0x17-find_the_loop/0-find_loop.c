aaca9ddd61ee7528787f409d61536c180bda5163
TMcMac <timmcmackenjr@gmail.com>
2021-10-06 21:46:23 +0000

    Initial commit

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * find_listint_loop - a function to search for a loop in
+ * singly linked list
+ * @head: a pointer to the head of a list
+ * Return: The address to the node where a loop starts or
+ * NULL if there is no loop. 
+ */
+
+
+listint_t *find_listint_loop(listint_t *head)
+{
+  listint_t *tort;
+  listint_t *hare;
+
+  tort = hare = head;
+
+  if (head == NULL || head->next == NULL) {
+    return (NULL);
+  }
+  
+  while(hare->next && hare->next->next) {
+    tort = tort->next;
+    hare = hare->next->next;
+    if (tort == hare) {
+      tort = head;
+      while (tort->next != head) {
+	tort = tort->next;
+      }
+      return (tort);
+    }
+  }
+  return (NULL);
+}
---
+++ 0x17-find_the_loop/README.md
@@ -0,0 +1,14 @@
+# 0x17. Find the Loop
+
+## Resources:books:
+Read or watch:
+
+---
+## Learning Objectives:bulb:
+What you should learn from this project:
+
+---
+---
+
+## Author
+* **Tim McMacken** - [TMcMac](https://github.com/TMcMac)
