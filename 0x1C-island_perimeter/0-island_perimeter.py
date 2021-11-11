f345bf2edf4b1674b1a58463022941ae1e748529
TMcMac <timmcmackenjr@gmail.com>
2021-11-11 20:28:55 +0000

    initial commit

---
+++ 0x1C-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python3
+
+def island_perimeter(grid):
+    row = 0
+    perimeter = 0
+    for row in range(len(grid)):
+        col = 0
+        for col in range(len(row)):
+            if grid[row, col] == 0:
+                continue
+            else:
+                if row == 0:
+                    perimeter += 1
+                elif row == (len(grid) - 1):
+                    perimeter += 1
+                if col == 0:
+                    perimeter += 1
+                elif col == (len(row) - 1):
+                    perimeter += 1
+                if grid[row, (col - 1)] == 0:
+                    perimeter += 1
+                if grid[row, (col + 1)] == 0:
+                    perimeter += 1
+    return perimeter
---
+++ 0x1C-island_perimeter/README.md
@@ -0,0 +1,14 @@
+# 0x1C. Island Perimeter
+
+## Resources:books:
+Read or watch:
+
+---
+## Learning Objectives:bulb:
+What you should learn from this project:
+
+---
+---
+
+## Author
+* **Tim McMacken** - [TMcMac](https://github.com/TMcMac)
