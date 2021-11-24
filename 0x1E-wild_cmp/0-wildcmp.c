565af4f6b28ac87384ad5e8df096bb22a5fe32bb
TMcMac <timmcmackenjr@gmail.com>
2021-11-24 03:16:07 +0000

    initial commit

---
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -0,0 +1,81 @@
+#include "holberton.h"
+/**
+ * wildcmp: a function to compare two strings to
+ * determin if they are exactly the same. A string
+ * can contain a wild card that is equal to any char
+ * in any quantity, so 'hello' and 'h*o' are considered
+ * as exact matches.
+ * @s1: a string
+ * @s2: a string that can contain the wildcard *
+ * Return: 1 if exactly the same, 0 if not
+ */
+
+
+int wildcmp(char *s1, char *s2)
+{
+  // int s1pos, s2pos, s2posnext;  
+  int s1len, s2len;
+
+  // if either string is null or empty it's game over
+  if(s1 == NULL || s2 == NULL)
+      return 0;
+  s1len = _strlen(s1);
+  s2len = _strlen(s2);
+  if(s1len == 0 || s2len == 0)
+      return 0;
+
+  // Quick check of first and last positions
+  // if s2 is just an * it will match any s1
+  if(s1[0] != s2[0] && s2[0] != '*')
+    return 0;
+  else if(s2len == 1 && s2[0] == '*')
+      return 1;
+  else if(allstar(s2, s2len))
+      return 1;
+  else if(s1[s1len] != s2[s2len] && s2[s2len] != '*')
+    return 0;
+
+  // s1pos = 0;
+  // s2pos = 0;
+  // s2posnext = 1;
+  
+  return 0;
+}
+
+
+/**
+ * _strlen - a function to get the len of a string
+ * @string: a pointer to the start of a string
+ * Return: an int for length
+ */
+
+
+int _strlen(char *string)
+{
+  int count = 0;
+
+  while(string[count] != '\0')
+    {
+      count++;
+    }
+  return (count);
+}
+
+
+/**
+ * allstar - a check of if a string is all ***
+ * @string: a string, a pointer to a string
+ * Return: 1 if all starts, 0 if not
+ */
+int allstar(char* string, int strlen)
+{
+  int i;
+  
+  for(i = 0; i < strlen; i++)
+    {
+      if(string[i] != '*')
+	return 0;
+		 
+    }
+  return 1;
+}
---
+++ 0x1E-wild_cmp/holberton.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_N
+#include <stdio.h>
+
+int wildcmp(char *s1, char *s2);
+int _strlen(char *string);
+int allstar(char* string, int strlen);
+
+#endif
