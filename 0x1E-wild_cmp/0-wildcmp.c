114905ea875f82802d687147fa2494b8af8b9e76
Tim <timmcmackenjr@gmail.com>
2021-11-24 17:14:59 -0600

    Betty Fixes

--- 0x1E-wild_cmp/0-wildcmp.c
+++ 0x1E-wild_cmp/0-wildcmp.c
@@ -17,7 +17,7 @@ int wildcmp(char *s1, char *s2)
 
 	/* if either string is null or empty it's game over */
 	if (s1 == NULL || s2 == NULL)
-		return 0;
+		return (0);
 	s1len = _strlen(s1);
 	s2len = _strlen(s2);
 	if (s1len == 0 || s2len == 0)
@@ -26,13 +26,13 @@ int wildcmp(char *s1, char *s2)
 	/* Quick check of first and last positions */
 	/* if s2 is just an * it will match any s1 */
 	if (s1[0] != s2[0] && s2[0] != '*')
-		return 0;
+		return (0);
 	else if (s2len == 1 && s2[0] == '*')
-		return 1;
+		return (1);
 	else if (allstar(s2, s2len))
 		return 1;
 	else if (s1[s1len] != s2[s2len] && s2[s2len] != '*')
-		return 0;
+		return (0);
 
 	check = full_check(s1, s2);
 	return (check);
@@ -50,7 +50,7 @@ int _strlen(char *string)
 {
 	int count = 0;
 
-	while(string[count] != '\0')
+	while (string[count] != '\0')
 		count++;
 	return (count);
 }
@@ -63,13 +63,15 @@ int _strlen(char *string)
  */
 
 
-int allstar(char* string, int strlen)
+int allstar(char *string, int strlen)
 {
 	int i;
 	
-	for(i = 0; i < strlen; i++)
-	    if(string[i] != '*')
+	for (i = 0; i < strlen; i++)
+	{
+		if (string[i] != '*')
 			return (0);
+	}
 	return (1);
 }
 
@@ -82,7 +84,7 @@ int allstar(char* string, int strlen)
  */
 
 
-int full_check(char* s1, char* s2)
+int full_check(char *s1, char *s2)
 {
 	/* If we hit the end of both strings, they match */
 	if(*s1 == '\0' && *s2 == '\0')
