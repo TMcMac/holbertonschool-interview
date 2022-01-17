af04d42ab6935fc14e3bcb1c4711dbcbafdd0451
Tim <timmcmackenjr@gmail.com>
2022-01-17 15:18:43 -0800

    Initial commit)

---
+++ 0x22./0-prime_game.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+    i = maria = ben = 0
+
+    if (x < 1):
+        return None
+    # print("Rounds: {}\nNums: {}".format(x, nums))
+    for i in range(0, x):
+        # print("Round: {}".format(i + 1))
+        if i >= len(nums):
+            break
+        else:
+            curRound = list(range(1, nums[i] + 1))
+            # print("Current List: {}".format(curRound))
+            if len(curRound) is 1:
+                # print("List of only 1 - point to Ben")
+                ben += 1
+                continue
+            else:
+                m = b = 0
+                turn = 2
+                while (len(curRound) > 1):
+                    num = curRound[1]
+                    # if (turn % 2) == 0:
+                    #    print("Maria picks {}".format(num))
+                    # else:
+                    #    print("Ben picks {}".format(num))
+                    curRound.remove(num)
+                    for i in curRound:
+                        if i % num is 0:
+                            # print("{} is removed from round".format(i))
+                            curRound.remove(i)
+                    if (turn % 2) is 0:
+                        m += 1
+                    else:
+                        b += 1
+                    turn += 1
+                    # print("Remaining in curRound: {}".format(curRound))
+                if (turn % 2) is 0:
+                    # print("Current round ended on Maria's turn, point to Ben")
+                    b += 1
+                else:
+                    # print("Current round ended on Ben's turn, point to Maria")
+                    m += 1
+        if m > b:
+            # print("End of Round: M = {} B = {}\n Maria Wins".format(m, b))
+            maria += 1
+        elif b > m:
+            # print("End of Round: M = {} B = {}\n Ben Wins".format(m, b))
+            ben += 1
+        else:
+            # print("M={} B={} DRAW".format(m, b))
+            continue
+            
+    if maria > ben:
+        # print("Wins M = {} B = {}".format(maria, ben))
+        return "Maria"
+    elif ben > maria:
+        # print("Wins M = {} B = {}".format(maria, ben))
+        return "Ben"
+    else:
+        return None
